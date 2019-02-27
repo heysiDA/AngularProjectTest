5e97c64960e768277accbcad344530f9e11c5e4a
Unknown <heysi.despaigne@cubacel.cu>
2019-02-27 10:07:16 -0500

    quitando lo que estaba en el proyecto de Hugo,para empezar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,85 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserHomeComponent } from './pages/user-home/user-home.component';
-import { RoleHomeComponent } from './pages/role-home/role-home.component';
-import { UserDetailsComponent } from './pages/user-details/user-details.component';
-import { UserAddComponent } from './pages/user-add/user-add.component';
-import { RoleDetailsComponent } from './pages/role-details/role-details.component';
-import { RoleAddComponent } from './pages/role-add/role-add.component';
-import { UserResolver } from './resolvers/user.resolver';
-import { RoleResolver } from './resolvers/role.resolver';
+
 
 const routes: Routes = [
-  {
-    path: 'users',
-    data: {
-      title: 'Users'
-    },
-    component: UserHomeComponent
-  },
-  {
-    path: 'users/:login/details',
-    data: {
-      title: 'User details'
-    },
-    component: UserDetailsComponent,
-    resolve: {
-      user: UserResolver
-    }
-  },
-  {
-    path: 'users/:login/edit',
-    data: {
-      title: 'User edit',
-      newRecord: 'false'
-    },
-    component: UserAddComponent,
-    resolve: {
-      user: UserResolver
-    }
-  },
-  {
-    path: 'users/add',
-    data: {
-      title: 'New user',
-      newRecord: 'true'
-    },
-    component: UserAddComponent
-  },
-  {
-    path: 'roles',
-    data: {
-      title: 'Roles'
-    },
-    component: RoleHomeComponent
-  },
-  {
-    path: 'roles/:name/details',
-    data: {
-      title: 'Role details'
-    },
-    component: RoleDetailsComponent,
-    resolve: {
-      role: RoleResolver
-    }
-  },
-  {
-    path: 'roles/:name/edit',
-    data: {
-      title: 'Role edit'
-    },
-    component: RoleAddComponent,
-    resolve: {
-      role: RoleResolver
-    }
-  },
-  {
-    path: 'roles/add',
-    data: {
-      title: 'New role'
-    },
-    component: RoleAddComponent
-  }
+ 
 ];
 
 @NgModule({
--- src/app/domain/user.ts
+++
@@ -1,8 +0,0 @@
-import { Role } from './role';
-
-export class User {
-  login: string;
-  name: string;
-  email: string;
-  roles: Role[];
-}
