fed38201b950215de0cb082498741eaca9fc84db
Unknown <heysi.despaigne@cubacel.cu>
2019-02-28 11:26:14 -0500

    primera vista donde se muestra una lista,usando *ngFor y *ngIf para datail vacio

--- src/app/components/menuheroes/menuheroes.component.ts
+++ src/app/components/menuheroes/menuheroes.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { from } from 'rxjs';
+import { Heroe } from 'src/app/domain/heroe';
+import { HEROES } from 'src/app/domain/mock-heroes';
 @Component({
   selector: 'app-menuheroes',
   templateUrl: './menuheroes.component.html',
   styleUrls: ['./menuheroes.component.css']
 })
 export class MenuheroesComponent implements OnInit {
-  hero = 'Windstorm';
-  
+  // heroe: Heroe = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+  heroes = HEROES;
+  selectedHeroe: Heroe;
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSelect(heroe: Heroe): void {
+    this.selectedHeroe = heroe;
+  }
 }
